f65bb6d7f627d5bc212310fd5ed6e5cf52792089
njoki <njerinjoki22@gmail.com>
2026-08-13 07:52:27 +0300

    refactor spline geometry to preserve endless corridor perspective

--- src/experiments/singularity/src/graphics/architectures/Tunnel.ts
+++ src/experiments/singularity/src/graphics/architectures/Tunnel.ts
@@ -1,14 +1,15 @@
+// src/graphics/architectures/Tunnel.ts
 import * as THREE from 'three';
 import { BaseArchitecture } from './BaseArchitecture';
 
 export const wormholeSpline = new THREE.CatmullRomCurve3(
   [
-    new THREE.Vector3(-10, 0, 10),
-    new THREE.Vector3(-5, 5, -10),
-    new THREE.Vector3(0, 0, -25),
-    new THREE.Vector3(5, -5, -10),
-    new THREE.Vector3(10, 0, 10),
-    new THREE.Vector3(0, 5, 25),
+    new THREE.Vector3(-18, 0, 45),
+    new THREE.Vector3(-10, 8, -25),
+    new THREE.Vector3(0, 0, -70),
+    new THREE.Vector3(10, -8, -25),
+    new THREE.Vector3(18, 0, 45),
+    new THREE.Vector3(0, 8, 70),
   ],
   true
 );
