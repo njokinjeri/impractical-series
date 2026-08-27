a4710b60715990ad8a9fdeb58073eb868a22cbe3
njoki <njerinjoki22@gmail.com>
2026-08-27 08:07:02 +0300

    add shader exports for vinyl rendering

---
+++ src/experiments/vinyl/src/shaders/vinylShaders.ts
@@ -0,0 +1,5 @@
+import vinylVertex from './vinylVertex.glsl?raw';
+import vinylFragment from './vinylFragment.glsl?raw';
+
+export const vinylVertexShader = vinylVertex;
+export const vinylFragmentShader = vinylFragment;
