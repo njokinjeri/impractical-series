37d7d26c870b9ac283975a9725b4adc1305b4806
njoki <njerinjoki22@gmail.com>
2026-08-27 07:49:30 +0300

    add vertex shader for vinyl rendering

---
+++ src/experiments/vinyl/src/shaders/vinylVertex.glsl
@@ -0,0 +1,11 @@
+varying vec3 vNormal;
+varying vec3 vWorldPosition;
+varying vec2 vUv;
+
+void main() {
+  vUv = uv;
+  vNormal = normalize(normalMatrix * normal);
+  vec4 worldPos = modelMatrix * vec4(position, 1.0);
+  vWorldPosition = worldPos.xyz;
+  gl_Position = projectionMatrix * viewMatrix * worldPos;
+}
